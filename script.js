624d308218de2ab3352204568ec56ad2f9786be0
Sidd <siddharthbudgude5@gmail.com>
2024-03-30 01:25:49 +0530

    basic structure of page and add slider to page for categories

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+// require([
+//      'jquery',
+//      'slick' ], function ($) {
+    $(document).ready(function () {
+        $('.slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 10,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: true,
+            
+            responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+    });
+// });
